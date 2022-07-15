fb63d3ac53b688a7be68cc0c6d5ff74e7c258eba
KTKT <a-b-c@kakao.com>
2022-07-15 18:19:09 +0900

    #1 스프링부트 게시판 만들기 - 프로젝트 생성

---
+++ src/main/java/com/example/boardbasic/BoardBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardBasicApplication.class, args);
+    }
+
+}
